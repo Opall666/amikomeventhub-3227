f8618e3f81a53ebe340d35795ac42f623eeea2b2
Opallimut <gbsopal@gmail.com>
2026-04-10 07:59:51 +0700

    Membuiat halaman kontak

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Kontak</title>
+    <!-- Tambahkan CSS untuk styling sederhana -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-slate-100 flex items-center justify-center h-screen">
+    <div class="bg-white p-8 rounded-x1 shadow-lg border border-slate-200 
+    text-center max-w-sm w-full">
+
+    <h1 class="text-2x1 font-bold text-slate-800 mb-2">Hubungi Kami</h1>
+    <p class="text-slate-500 mb-6">Email : admin@amikomeventhub.com</p>
+    <a href="/" class="inline-block bg-indigo-600 text-white 
+        font-semibold py-2 px-6 rounded-lg hover:bg-indigo-700 hover:shadow-md 
+        transition duration-300">
+            Kembali Ke Home
+        </a>
+    </div>
+</body>
+</html>
